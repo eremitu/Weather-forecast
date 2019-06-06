3abc6f8dbf81bf2c4c1cda1c7ce3a4e74e842adc
Eremitu <shvarz96@gmail.com>
2019-06-06 18:51:15 +0200

    Minor syntax improvements

--- src/dataService.js
+++ src/dataService.js
@@ -5,74 +5,25 @@ import _ from 'lodash';
 
 
 export class DataService extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        items: [],
-        input: '',
-      };
-      this.componentDidMount = this.componentDidMount.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      input: '',
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.props.input !== prevProps.props.input) {
+      console.log("АСТАНАВИСЬ")
+      this.search()
     }
+  }
 
-    componentDidUpdate(prevProps) {
-      console.log(this.props.props.input)
-      if (this.props.props.input !== prevProps.props.input) {
-        console.log(this.props.input)
-        this.search()
-      }
-    }
-
-    componentDidMount() {
-      new Promise(async (resolve) => {
-          await setTimeout(() => {
-            resolve(this.props)
-          }, 1000)
-        })
-        .then(
-          async () => {
-            await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.props.latitude}&lon=${this.props.props.longitude}&appid=${APIkey}`)
-              .then(async (res) => await res.json())
-              .then(
-                (result) => {
-                  console.log(result)
-                  this.setState({
-                    isLoaded: true,
-                    items: result
-                  });
-
-                },
-                (error) => {
-                  this.setState({
-                    isLoaded: true,
-                    error
-                  });
-                }
-              )
-          }
-        )
-    }
-
-  /*componentDidUpdate() {
-    let response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.props.input}&appid=${APIkey}`)
-      if (response.ok) { 
-        let json = response.json();
-        this.setState({
-          isLoaded: true,
-          items: json
-        });
-      } else {
-        this.setState({
-          isLoaded: true,
-          error: response.status
-        });
-        console.log("HTTP-Error: " + response.status);
-      }
-    
-  }*/
-  search() {
-
+  componentDidMount() {
     new Promise(async (resolve) => {
       await setTimeout(() => {
         resolve(this.props)
@@ -80,58 +31,58 @@ export class DataService extends React.Component {
     })
     .then(
       async () => {
-        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.props.input}&appid=${APIkey}`)
-          .then(async (res) => await res.json())
-          .then(
-            (result) => {
-              console.log(result)
-              this.setState({
-                isLoaded: true,
-                items: result
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.props.latitude}&lon=${this.props.props.longitude}&appid=${APIkey}`)
+        .then(async (res) => await res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
       }
     )
+  }
 
-
-
-/*
-      let response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.props.input}&appid=${APIkey}`)
-      if (response.ok) {
-        let json2 = response.json();
-        this.setState({
-          isLoaded: true,
-          items: json2
-        });
-        console.log(response);
-      } else {
-        this.setState({
-          isLoaded: true,
-          error: response.status
-        });
-
-        console.log("HTTP-Error: " + response.status);
+  search() {
+    new Promise(async (resolve) => {
+        await setTimeout(() => {
+          resolve(this.props)
+        }, 1000)
+    })
+    .then(
+      async () => {
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.props.input}&appid=${APIkey}`)
+        .then(async (res) => await res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
       }
-
-
-  console.log('errrrorrr')
-*/
-
-}
+    )
+  }
 
   render() {
     const {
-      error,
       isLoaded,
       items,
-      input
     } = this.state;
     if (this.state.error != null) {
       return <div className="container-fluid data text-center">API Server is not responding </div>;
@@ -157,15 +108,7 @@ export class DataService extends React.Component {
             Unable to get your GeoLocation or server went wrong
           </div> 
       );
+    }
+
   }
-
 }
-}
-
-// You are in {items.name ? items.name : 'nowhere'}.
-// Temperature now is {items.main.temp ? items.main.temp : '??'} F
-// Athmospheric pressure is {items.main.pressure ? items.main.pressure : 'nowhere'}
-
-// You are in {_.isUndefined(items) !== true ? items.name : 'nowhere'}.
-// Temperature now is {!_.isUndefined(items) !== true  ? items.main.temp : '??'} F
-// Athmospheric pressure is {!_.isUndefined(items) !== true  ? items.main.pressure : '??'} mbar 
